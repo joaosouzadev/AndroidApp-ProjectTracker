ea1cfa191cb425bcbb1f52518ab5d135b02364da
João Victor Souza <jvms3d@gmail.com>
2018-04-30 16:38:48 -0300

    Menu na Atividade Editor de Projetos

--- app/src/main/java/com/example/android/projecttracker/EditorActivity.java
+++ app/src/main/java/com/example/android/projecttracker/EditorActivity.java
@@ -2,8 +2,11 @@ package com.example.android.projecttracker;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -39,8 +42,7 @@ public class EditorActivity extends AppCompatActivity {
     private Spinner mProjectSpinner;
 
     /**
-     * Gender of the pet. The possible values are:
-     * 0 for unknown gender, 1 for male, 2 for female.
+     * Type of the Project
      */
     private String mProjectType = "";
 
@@ -98,5 +100,35 @@ public class EditorActivity extends AppCompatActivity {
                 mProjectType = "Tipo de Projeto não selecionado"; // Unknown
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu options from the res/menu/menu_editor.xml file.
+        // This adds menu items to the app bar.
+        getMenuInflater().inflate(R.menu.menu_editor, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // User clicked on a menu option in the app bar overflow menu
+        switch (item.getItemId()) {
+            // Respond to a click on the "Save" menu option
+            case R.id.action_save:
+                //inserProject();
+                finish();
+                return true;
+            // Respond to a click on the "Clear" menu option
+            case R.id.action_clear:
+                // Do nothing for now
+                return true;
+            // Respond to a click on the "Up" arrow button in the app bar
+            case android.R.id.home:
+                // Navigate back to parent activity (MainActivity)
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
